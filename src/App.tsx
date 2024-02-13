cbfcadae683967257d65272b833e425c790bc2db
KenM4sters <EHCKenway@outlook.com>
2024-02-13 17:56:47 +0000

    init :zap:

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='main-wrapper'>
+      <div>
+        <h1> App </h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
