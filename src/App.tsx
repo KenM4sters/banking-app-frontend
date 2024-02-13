33ee0ca7441efe206b23b1b1155803c76481691f
KenM4sters <EHCKenway@outlook.com>
2024-02-13 18:30:50 +0000

    initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Scrollbar from './components/Scrollbar'
 
 const App = () => {
   return (
     <main className='main-wrapper'>
-      <div>
-        <h1> App </h1>
+      <div className='main-container'>
+        <div className='header-wrapper'>
+          <Header />
+        </div>
+        <section>
+          
+        </section>
+        <div className='footer-wrapper'>
+          <Footer />
+        </div>
       </div>
+      <Scrollbar />
     </main>
   )
 }
