4f4f060f85e34b04cd5cfbd330cc17d77caf8387
KenM4sters <EHCKenway@outlook.com>
2024-02-15 17:13:23 +0000

    changed colors

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,39 +1,75 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
-const Account = ({data}) => {
+const Account = ({ data }) => {
+  const [activeCard, setActiveCard] = useState({});
+  const [activeCardColors, setActiveCardColors] = useState([{}]);
+  const [activeColorIndex, setActiveColorIndex] = useState(0);
+  
+  useEffect(() => {
+    setActiveCardColors([
+      {
+        backgroundColor: "#f60a4d",
+        boxShadow: "0px 0px 10px #f60a4d",
+      },
+      {
+        backgroundColor: "#9ad3fd",
+        boxShadow: "0px 0px 10px #9ad3fd",
+      },
+      {
+        backgroundColor: "#9ad3fd",
+        boxShadow: "0px 0px 10px #9ad3fd",
+      },
+      {
+        backgroundColor: "#9ad3fd",
+        boxShadow: "0px 0px 10px #9ad3fd",
+      },
+    ]);
+  }, []);
+
+
   return (
-    <div className='account-view-wrapper'>
-        <div className='account-view-container'>
-            <h3 className='account-view-header'> Account View </h3>
-            <p className='account-view-para'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi a rerum tenetur sapiente accusantium quam 
-                dignissimos modi ullam facere adipisci commodi 
-                provident pariatur ex totam neque quibusdam, quos cupiditate! Voluptatem!
-            </p>
+    <div className="account-view-wrapper">
+      <div className="account-view-container">
+        <h3 className="account-view-header"> Account View </h3>
+        <p className="account-view-para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi a
+          rerum tenetur sapiente accusantium quam dignissimos modi ullam facere
+          adipisci commodi provident pariatur ex totam neque quibusdam, quos
+          cupiditate! Voluptatem!
+        </p>
+      </div>
+      <div className="account-cards-navbar">
+        {data?.content?.map((data, index: number) => (
+          <span
+            onClick={() => {
+              toggleActiveCard(10);
+            }}
+            className="account-cards"
+            key={index}
+            style={activeCardColors[index]}
+          >
+            Card [{index}]
+          </span>
+        ))}
+      </div>
+      <div className="account-overview">
+        <form className="account-options-list">
+          <input />
+          <input />
+          <input />
+          <input />
+        </form>
+        <div className="account-card-details">
+          <h3 className="account-card-details-header">Card Number</h3>
+          <h3 className="account-card-details-header"> Card Name</h3>
+          <h3 className="account-card-details-header"> Balance </h3>
         </div>
-        <div className='account-cards-navbar'>
-            {data?.content?.map((data, index) => (
-                <span className='account-cards' key={index}> Card [{index}]</span>
-            ))}
-        </div>
-        <div className='account-overview'>
-            <form className='account-options-list'>
-                <input />
-                <input />
-                <input />
-                <input />
-            </form>
-            <div className='account-card-details'>
-                <h3 className='account-card-details-header'>Card Number</h3>
-                <h3 className='account-card-details-header'> Card Name</h3>
-                <h3 className='account-card-details-header'> Balance </h3>
-            </div>
-        </div>
-        <div className='transaction-history'>
-            <h3 className='transaction-history-header'>Transaction history</h3>
-        </div>
+      </div>
+      <div className="transaction-history">
+        <h3 className="transaction-history-header">Transaction history</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
